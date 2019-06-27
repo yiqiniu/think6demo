7d511691be91b724056f55bea0b457f30bd6a2c6
gjianbo <gjianbo@qq.com>
2019-06-27 11:23:57 +0800

    修复不支持Class为表名的

--- extend/yiqiniu/console/command/ModelAll.php
+++ extend/yiqiniu/console/command/ModelAll.php
@@ -20,12 +20,12 @@ class ModelAll extends Make
     protected $type = 'Command';
 
     // 基础Model的名称
-    protected $baseModel='BaseModel';
+    protected $baseModel = 'BaseModel';
 
     //配置模板
     protected $stubs = [
-        'basemodel'=>'basemodel',
-        'model'=>'model',
+        'basemodel' => 'basemodel',
+        'model' => 'model',
     ];
 
     protected function configure()
@@ -51,7 +51,7 @@ class ModelAll extends Make
             return;
         }
         //select table_name,table_comment from information_schema.tables where table_schema='yiqiniu_new';
-        $tablelist = Db::connect($default)->table('information_schema.tables')
+        $tablelist = Db::connect($default ?: $connect)->table('information_schema.tables')
             ->where('table_schema', $connect['database'])
             ->field('table_name as name,table_comment as comment')
             ->select();
@@ -64,8 +64,8 @@ class ModelAll extends Make
         } else {
             $dirname = $apppath . 'model\\';
         }
-        if(!file_exists($dirname)){
-            mkdir($dirname,0644,true);
+        if (!file_exists($dirname)) {
+            mkdir($dirname, 0644, true);
         }
         // 获取生成空间的名称
         $namespace = $this->getNamespace($name);
@@ -74,39 +74,45 @@ class ModelAll extends Make
 
         $stubs = $this->getStub();
         // 写入基本的Model类
-        $basemodel_file = $dirname.$this->baseModel.'.php';
+        $basemodel_file = $dirname . $this->baseModel . '.php';
 
-        if(!file_exists($basemodel_file)){
+        if (!file_exists($basemodel_file)) {
             $basemodel = file_get_contents($stubs['basemodel']);
-            file_put_contents($basemodel_file,str_replace(['{%namespace%}','{%className%}',], [
+            file_put_contents($basemodel_file, str_replace(['{%namespace%}', '{%className%}',], [
                 $namespace,
                 $this->baseModel,
             ], $basemodel));
         }
 
 
-
         // 生成所有的类
-        $prefix_len  = strlen($connect['prefix']);
+        $prefix_len = strlen($connect['prefix']);
 
         $model_stub = file_get_contents($stubs['model']);
 
-        foreach ($tablelist as $k=>$table){
-            $class_name = $this->app->parseName(substr($table['name'],$prefix_len),1,true);
+        foreach ($tablelist as $k => $table) {
+            $class_name = $this->app->parseName(substr($table['name'], $prefix_len), 1, true);
+            // 如果是表名是class的改为ClassModel
 
-            $model_file = $dirname.$class_name.'.php';
-            if(!file_exists($model_file)){
-                file_put_contents($model_file,str_replace(['{%namespace%}','{%className%}','{%comment%}',], [
+            $tablename = '';
+            if ($class_name == 'Class') {
+                $class_name = 'ClassModel';
+                $tablename = "protected \$name='" . substr($table['name'], $prefix_len) . "';";
+            }
+            $model_file = $dirname . $class_name . '.php';
+            if (!file_exists($model_file)) {
+                file_put_contents($model_file, str_replace(['{%namespace%}', '{%className%}', '{%comment%}', '{%tablename%}'], [
                     $namespace,
                     $class_name,
-                    $table['comment']
+                    $table['comment'],
+                    $tablename
                 ], $model_stub));
             }
 
         }
 
 
-        $output->writeln('<info>' . $this->type . ':'  . 'All Table Model created successfully.</info>');
+        $output->writeln('<info>' . $this->type . ':' . 'All Table Model created successfully.</info>');
 
 
     }
@@ -119,9 +125,9 @@ class ModelAll extends Make
     protected function getStub()
     {
 
-        foreach ($this->stubs as $key=>$filename){
+        foreach ($this->stubs as $key => $filename) {
 
-            $this->stubs[$key] = __DIR__ . DIRECTORY_SEPARATOR . 'stubs' . DIRECTORY_SEPARATOR . $filename.'.stub';
+            $this->stubs[$key] = __DIR__ . DIRECTORY_SEPARATOR . 'stubs' . DIRECTORY_SEPARATOR . $filename . '.stub';
         }
         return $this->stubs;
     }
