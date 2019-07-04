8674efdd9b3a02293a6c714dffdf559a729cd2b3
gjianbo <gjianbo@qq.com>
2019-07-04 15:45:01 +0800

    使用了 tp的队列

---
+++ app/admin/queue/Job1.php
@@ -0,0 +1,71 @@
+<?php
+
+
+namespace app\admin\queue;
+
+
+use think\queue\Job;
+
+/**
+ * Class Job1
+ * @package app\admin\queue
+ */
+class Job1 extends Job
+{
+
+    /**
+     * Get the job identifier.
+     *
+     * @return string
+     */
+    public function getJobId()
+    {
+        // TODO: Implement getJobId() method.
+    }
+
+    /**
+     * Get the number of times the job has been attempted.
+     * @return int
+     */
+    public function attempts()
+    {
+        // TODO: Implement attempts() method.
+    }
+
+    /**
+     * Get the raw body string for the job.
+     * @return string
+     */
+    public function getRawBody()
+    {
+        // TODO: Implement getRawBody() method.
+    }
+
+    public function task1(Job $job,$data){
+        sleep(2);
+        echo time()."\r\n";
+        echo 'task1'."\r\n";
+
+        print_r($data);
+        $job->delete();
+    }
+    public function task2(Job $job,$data){
+        echo 'task2'."\r\n";
+        print_r($data);
+        $job->delete();
+    }
+
+    public function task3(Job $job,$data){
+        echo 'task3'."\r\n";
+        print_r($data);
+    }
+
+   /* public function failed($data, \Exception $e){
+
+        echo 'task3'."\r\n";
+        print_r($data);
+        echo $e->getMessage()."\r\n";
+
+
+    }*/
+}
